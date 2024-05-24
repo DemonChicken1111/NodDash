6fb2fcde642249d144f00ffdcde65da38bc91c9a
DemonChicken1111 <Eyeofakimoto@gmail.com>
2024-05-24 17:05:57 -0400

    First Commit

---
+++ JSONExporter.py
@@ -0,0 +1,22 @@
+import json
+import requests
+
+BaseURL = "https://blockchain-gateway-test.nursery.reitnorf.com/"
+SmartDeployableURL = "smartdeployables/"
+TypeURL = "types/"
+
+def ExportJSONFile(FileName, URLParam, ID):
+
+	if URLParam == "-S":
+		URLAddon = SmartDeployableURL 
+	if URLParam == "-T":
+		URLAddon = TypeURL 	
+	else:
+		print(URLParam + " is not a vaild parameter.")
+
+	r = requests.get(BaseURL+URLAddon+ID)
+	request = r.json()
+	
+	with open(FileName, "w") as outfile:
+		outfile.write(json.dumps(request, indent=4))
+
