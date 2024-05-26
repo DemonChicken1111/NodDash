99f10a58cec8de594bb6e82a69af11dee6af1323
DemonChicken1111 <Eyeofakimoto@gmail.com>
2024-05-26 12:10:01 -0400

    Fixed Basic Logging

--- Logging.py
+++ Logging.py
@@ -23,19 +23,24 @@ class Logger():
 				fileName = t.strftime("%c") + " " + obj[y]
 				JSONImporter.ImportJSONFile(fileName, obj[y])
 		
-		logging = "SmartDeployables fetched"
+		global logging
+		logging = "Smart Deployables Fetched"
 
-	def Logging(): #Still wont work :(, gets stuck in else
+	def Logging(): 
 
+		global logging
 		logFile = open("log.txt", "a")
 		logEntry = logging
 
 		if logEntry != "":
-			logFile.write(logEntry)
-			print("logged")
-		else:
-			print(logEntry + "else")
+			logFile.write(logEntry + ": " + t.strftime("%c") + "\n")
+		#	print("logged")
 			pass
+		else:
+		#	print(logEntry + "else")
+			pass
+
+		logging = ""
 
 
 def streamTask(lock):
@@ -43,7 +48,7 @@ def streamTask(lock):
 
 	while True:
 		lock.acquire()
-		Logger().JSONStreamer()
+		Logger.JSONStreamer()
 		lock.release()
 		print("Sleeping..")
 		sleep(10)
@@ -51,13 +56,20 @@ def streamTask(lock):
 
 def logTask(lock):
 	#Task for a thread, Calls Logging function
-
+	
 	while True:
 		lock.acquire()
-		Logger().Logging()
-		lock.release()
+
+		if threading.current_thread().name != "thread1":
+			Logger.Logging()
+			lock.release()
 
 def main():
+
+	global logging
+
+	logging = ""
+
 	Logs = Logger()
 	lock = threading.Lock()
 
